1c8264a5c1d1915c17dddb0bd07cdc49ca5bef31
chirag <kerkarc509@gmail.com>
2025-05-02 17:18:06 +0530

    Cors changes pushed.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
   // await app.listen(process.env.PORT ?? 3000);
   const config = new DocumentBuilder()
     .setTitle('Employee Management API')
